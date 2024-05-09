c237f5e5c94f60d29b38e35dfb646b3b082b4128
Victor Luis <101286309+Vitin-Luis@users.noreply.github.com>
2024-05-09 08:19:19 -0300

    Aula do dia 09/05/2024

---
+++ Aula20240509/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Aula20240509
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxEstados = new System.Windows.Forms.TextBox();
+            this.btnAddEstados = new System.Windows.Forms.Button();
+            this.comboBoxListaEstados = new System.Windows.Forms.ComboBox();
+            this.listBoxEstados = new System.Windows.Forms.ListBox();
+            this.btnCopiarEstados = new System.Windows.Forms.Button();
+            this.btnCopiarSelecionados = new System.Windows.Forms.Button();
+            this.listBoxEstadosSelecionados = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBoxEstados
+            // 
+            this.textBoxEstados.Location = new System.Drawing.Point(179, 41);
+            this.textBoxEstados.Name = "textBoxEstados";
+            this.textBoxEstados.Size = new System.Drawing.Size(100, 20);
+            this.textBoxEstados.TabIndex = 0;
+            this.textBoxEstados.TextChanged += new System.EventHandler(this.textBoxEstados_TextChanged);
+            // 
+            // btnAddEstados
+            // 
+            this.btnAddEstados.Location = new System.Drawing.Point(44, 38);
+            this.btnAddEstados.Name = "btnAddEstados";
+            this.btnAddEstados.Size = new System.Drawing.Size(75, 23);
+            this.btnAddEstados.TabIndex = 1;
+            this.btnAddEstados.Text = "Adicionar";
+            this.btnAddEstados.UseVisualStyleBackColor = true;
+            this.btnAddEstados.Click += new System.EventHandler(this.btnAddEstados_Click);
+            // 
+            // comboBoxListaEstados
+            // 
+            this.comboBoxListaEstados.FormattingEnabled = true;
+            this.comboBoxListaEstados.Location = new System.Drawing.Point(44, 98);
+            this.comboBoxListaEstados.Name = "comboBoxListaEstados";
+            this.comboBoxListaEstados.Size = new System.Drawing.Size(235, 21);
+            this.comboBoxListaEstados.TabIndex = 2;
+            this.comboBoxListaEstados.SelectedIndexChanged += new System.EventHandler(this.comboBoxListaEstados_SelectedIndexChanged);
+            // 
+            // listBoxEstados
+            // 
+            this.listBoxEstados.FormattingEnabled = true;
+            this.listBoxEstados.Location = new System.Drawing.Point(44, 188);
+            this.listBoxEstados.Name = "listBoxEstados";
+            this.listBoxEstados.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBoxEstados.Size = new System.Drawing.Size(120, 95);
+            this.listBoxEstados.TabIndex = 3;
+            this.listBoxEstados.SelectedIndexChanged += new System.EventHandler(this.listBoxEstados_SelectedIndexChanged);
+            // 
+            // btnCopiarEstados
+            // 
+            this.btnCopiarEstados.Location = new System.Drawing.Point(44, 141);
+            this.btnCopiarEstados.Name = "btnCopiarEstados";
+            this.btnCopiarEstados.Size = new System.Drawing.Size(75, 23);
+            this.btnCopiarEstados.TabIndex = 4;
+            this.btnCopiarEstados.Text = "Copiar";
+            this.btnCopiarEstados.UseVisualStyleBackColor = true;
+            this.btnCopiarEstados.Click += new System.EventHandler(this.btnCopiarEstados_Click);
+            // 
+            // btnCopiarSelecionados
+            // 
+            this.btnCopiarSelecionados.Location = new System.Drawing.Point(215, 141);
+            this.btnCopiarSelecionados.Name = "btnCopiarSelecionados";
+            this.btnCopiarSelecionados.Size = new System.Drawing.Size(138, 23);
+            this.btnCopiarSelecionados.TabIndex = 5;
+            this.btnCopiarSelecionados.Text = "Copiar Selecionados";
+            this.btnCopiarSelecionados.UseVisualStyleBackColor = true;
+            this.btnCopiarSelecionados.Click += new System.EventHandler(this.btnCopiarSelecionados_Click);
+            // 
+            // listBoxEstadosSelecionados
+            // 
+            this.listBoxEstadosSelecionados.FormattingEnabled = true;
+            this.listBoxEstadosSelecionados.Location = new System.Drawing.Point(215, 188);
+            this.listBoxEstadosSelecionados.Name = "listBoxEstadosSelecionados";
+            this.listBoxEstadosSelecionados.Size = new System.Drawing.Size(138, 95);
+            this.listBoxEstadosSelecionados.TabIndex = 6;
+            this.listBoxEstadosSelecionados.SelectedIndexChanged += new System.EventHandler(this.listBoxEstadosSelecionados_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBoxEstadosSelecionados);
+            this.Controls.Add(this.btnCopiarSelecionados);
+            this.Controls.Add(this.btnCopiarEstados);
+            this.Controls.Add(this.listBoxEstados);
+            this.Controls.Add(this.comboBoxListaEstados);
+            this.Controls.Add(this.btnAddEstados);
+            this.Controls.Add(this.textBoxEstados);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxEstados;
+        private System.Windows.Forms.Button btnAddEstados;
+        private System.Windows.Forms.ComboBox comboBoxListaEstados;
+        private System.Windows.Forms.ListBox listBoxEstados;
+        private System.Windows.Forms.Button btnCopiarEstados;
+        private System.Windows.Forms.Button btnCopiarSelecionados;
+        private System.Windows.Forms.ListBox listBoxEstadosSelecionados;
+    }
+}
+
---
+++ Aula20240509/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula20240509
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddEstados_Click(object sender, EventArgs e)
+        {
+            // Verifica se Algo foi digitado, se estiver vazio exibe uma mensagem de erro, do contrário adiciona o estado no combox
+            if (textBoxEstados.Text.ToString() ==  "" || textBoxEstados.Text.ToString() == " ")
+            {
+                MessageBox.Show("Você não digitou um estado", "ALERTA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                comboBoxListaEstados.Items.Add(textBoxEstados.Text.ToString().ToUpper());
+                textBoxEstados.Clear();
+            }
+        
+        
+        }
+
+        private void textBoxEstados_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxListaEstados_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnCopiarEstados_Click(object sender, EventArgs e)
+        {
+            // Adiciona cada item do comBox na lista
+            for(int i = 0; i < comboBoxListaEstados.Items.Count; i++)
+            {
+                listBoxEstados.Items.Add(comboBoxListaEstados.Items[i].ToString());
+            }
+        }
+
+        private void listBoxEstados_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCopiarSelecionados_Click(object sender, EventArgs e)
+        {
+            // Adiciona os items selecionados na lista para a outra lista
+            for(int i = 0; i < listBoxEstados.SelectedItems.Count; i++)
+            {
+                listBoxEstadosSelecionados.Items.Add(listBoxEstados.SelectedItems[i]);
+            }
+    
+        }
+
+        private void listBoxEstadosSelecionados_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
